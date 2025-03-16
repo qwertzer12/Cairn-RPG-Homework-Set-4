2cf05b5d66421cca8eeb3a4cbb3d3e8901f39453
qwertzer12 <qwertzer12@outlook.com>
2025-03-16 17:54:19 -0400

    Add JUnit 5 test configuration and initial CharacterTest class

---
+++ tests/CharacterTest.java
@@ -0,0 +1,47 @@
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+
+class CharacterTest {
+
+    @Test
+    void runAway() {
+
+    }
+
+    @Test
+    void info() {
+    }
+
+    @Test
+    void getName() {
+    }
+
+    @Test
+    void setName() {
+    }
+
+    @Test
+    void getHealth() {
+    }
+
+    @Test
+    void setHealth() {
+    }
+
+    @Test
+    void getAttackPower() {
+    }
+
+    @Test
+    void setAttackPower() {
+    }
+
+    @Test
+    void isAlive() {
+    }
+
+    @Test
+    void setAlive() {
+    }
+}
