323e0a751d992f7178869cedbe0d9c040f6382b2
qwertzer12 <qwertzer12@outlook.com>
2025-03-18 12:50:25 -0400

    Enhance World class with weapon and healing item management

--- src/Data/World.java
+++ src/Data/World.java
@@ -1,29 +1,57 @@
 package Data;
 
+import java.util.ArrayList;
+
 public class World {
-    private String gameName;
-    private String publisher;
+    private final String gameName = "Cairn RPG";
+    private final String publisher = "AJ Chabin, Samuel Chapin";
+    private ArrayList<Weapon> weapons = new ArrayList<>();
+    private ArrayList<HealingItem> healingItems = new ArrayList<>();
 
 
     public World() {
-        this.setGameName("Cairn RPG");
-        this.setPublisher("AJ Chabin, Samuel Chapin");
+        weapons.add(new Weapon(weapons.size(), "Wooden Sword", 5));
+        weapons.add(new Weapon(weapons.size(), "Iron Sword", 10));
+        weapons.add(new EnchantedWeapon(weapons.size(), "Fire Sword", 8, 4));
+
+        weapons.add(new Weapon(weapons.size(), "Wooden Axe", 7));
+        weapons.add(new Weapon(weapons.size(), "Iron Axe", 12));
+        weapons.add(new EnchantedWeapon(weapons.size(), "Ice Axe", 9, 5));
+
+        weapons.add(new Weapon(weapons.size(), "Wooden Dagger", 5));
+        weapons.add(new Weapon(weapons.size(), "Iron Dagger", 8));
+        weapons.add(new EnchantedWeapon(weapons.size(), "Poison Dagger", 6, 9));
+
+
+        healingItems.add(new HealingItem(healingItems.size(), "Healing Potion", 10));
+        healingItems.add(new HealingItem(healingItems.size(), "Super Potion", 20));
+        healingItems.add(new HealingItem(healingItems.size(), "Mega Potion", 50));
     }
 
     public String getGameName() {
         return gameName;
     }
 
-    public void setGameName(String gameName) {
-        this.gameName = gameName;
-    }
-
     public String getPublisher() {
         return publisher;
     }
 
-    public void setPublisher(String publisher) {
-        this.publisher = publisher;
+    public ArrayList<Weapon> getWeapons() {
+        return weapons;
+    }
+
+    public Weapon randomWeapon() {
+        return weapons.get((int)(Math.random() * weapons.size()));
+    }
+
+    public ArrayList<HealingItem> getHealingItems() {
+        return healingItems;
+    }
+
+    public HealingItem randomHealingItem() {
+        return healingItems.get((int)(Math.random() * healingItems.size()));
     }
 }
+
+
 
