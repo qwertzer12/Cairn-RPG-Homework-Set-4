437b3f797aa577c2bfe6184cf8e9086359b475d6
SChapin67 <sc547@cairn.edu>
2025-03-18 20:23:36 -0400

    Tests Done Ready to Submit!

---
+++ tests/WorldTest.java
@@ -0,0 +1,22 @@
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class WorldTest {
+
+    @Test
+    void getGameName() {
+    }
+
+    @Test
+    void setGameName() {
+    }
+
+    @Test
+    void getPublisher() {
+    }
+
+    @Test
+    void setPublisher() {
+    }
+}
