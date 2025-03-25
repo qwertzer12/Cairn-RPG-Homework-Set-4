acda5e91ec7b479c6b5990c9e5ec08148efed609
SChapin67 <sc547@cairn.edu>
2025-03-25 14:06:59 -0400

    JUnit Tests have been completed

--- tests/ItemTest.java
+++ tests/ItemTest.java
@@ -1,26 +1,41 @@
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class ItemTest {
+    private Item item;
+
+    @BeforeEach
+    void setUp() {
+        item = new Item("Potion", 50);
+    }
 
     @Test
     void info() {
+        String expected = "Item{name='Potion', healingPower=50}";
+        assertEquals(expected, item.Info());
     }
 
     @Test
     void getName() {
+        assertEquals("Potion", item.getName());
     }
 
     @Test
     void setName() {
+        item.setName("Elixir");
+        assertEquals("Elixir", item.getName());
     }
 
     @Test
     void getHealingPower() {
+        assertEquals(50, item.getHealingPower());
     }
 
     @Test
     void setHealingPower() {
+        item.setHealingPower(100);
+        assertEquals(100, item.getHealingPower());
     }
 }
--- tests/WorldTest.java
+++ tests/WorldTest.java
@@ -6,17 +6,29 @@ class WorldTest {
 
     @Test
     void getGameName() {
+        World world = new World();
+        world.setGameName("Adventure World");
+        assertEquals("Adventure World", world.getGameName(), "Game name should be 'Adventure World'");
     }
 
     @Test
     void setGameName() {
+        World world = new World();
+        world.setGameName("Mystic Quest");
+        assertEquals("Mystic Quest", world.getGameName(), "Game name should be 'Mystic Quest'");
     }
 
     @Test
     void getPublisher() {
+        World world = new World();
+        world.setPublisher("Game Studio");
+        assertEquals("Game Studio", world.getPublisher(), "Publisher should be 'Game Studio'");
     }
 
     @Test
     void setPublisher() {
+        World world = new World();
+        world.setPublisher("Epic Games");
+        assertEquals("Epic Games", world.getPublisher(), "Publisher should be 'Epic Games'");
     }
 }
